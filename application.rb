10514c15b26951fe743ef2743cd0f384d11ea0ff
WhpHenry <527whp@gmail.com>
2017-02-13 12:35:27 +0800

    modified params

--- application.rb
+++ application.rb
@@ -1,49 +1,62 @@
-before do
-  content_type :json
-  headers 'Access-Control-Allow-Origin' => '*',
-          'Access-Control-Allow-Methods' => ['POST']
-end
+set :public, Proc.new { File.join(root, "_site") }
 
-set :protection, false
-set :public_dir, Proc.new { File.join(root, "") }
-
-post '/send_email' do
+post '/send' do  
   if recaptcha_valid?
-    res = Pony.mail(
-      :from => params[:name] + "<" + params[:email] + ">",
-      :to => 'info@sync4genes.org',
-      :subject => "null of filter" + params[:subject],
-      :body => params[:message],
-      :via => :smtp,
-      :via_options => {
-        :address              => 'smtp.sendgrid.net',
-        :port                 => '587',
-        :enable_starttls_auto => true,
-        :user_name            => ENV['s4g_mail'],
-        :password             => ENV['sync4gnens'],
-        :authentication       => :plain,
-        :domain               => 'heroku.com'
-      })
-    content_type :json
-    if res
+    session[:captcha] = true
+    { :message => 'success' }.to_json
+  else
+    session[:captcha] = false
+    { :message => 'failure' }.to_json
+  end
+end
+
+post '/send_email' do  
+    require 'pony'
+    require 'json'
+
+    if session[:captcha]
+      session[:captcha] = false
+      res = Pony.mail(
+    :from => params[:name] + "<" + params[:email] + ">",
+    :to => '315875452@qq.com',
+    # :to => 'info@sync4genes.org',
+    :subject => "Message from your awesome website :)",
+    :body => params[:message],
+    :port => '587',
+    :via => :smtp,
+    :via_options => {
+      :address              => 'smtp.sendgrid.net',
+      :port                 => '587',
+      :enable_starttls_auto => true,
+      :user_name            => ENV['s4g_mail'],
+      :password             => ENV['sync4gnens'],
+      :authentication       => :plain,
+      :domain               => 'heroku.com'
+    })
+      content_type :json
+      if res
       { :message => 'success' }.to_json
+      else
+      { :message => 'failure' }.to_json
+      end
     else
-      { :message => 'failure_email' }.to_json
+      { :message => 'failure' }.to_json
     end
-  else
-    { :message => 'failure_captcha' }.to_json
-  end
 end
 
-not_found do
-  File.read('404.html')
+before do  
+    response.headers['Cache-Control'] = 'public, max-age=36000'
 end
 
-get '/*' do
-  file_name = "#{request.path_info}/_layouts/home.html".gsub(%r{\/+},'/')
-  if File.exists?(file_name)
+not_found do  
+    File.read('_site/404.html')
+end
+
+get '/*' do  
+    file_name = "_site#{request.path_info}/index.html".gsub(%r{\/+},'/')
+    if File.exists?(file_name)
     File.read(file_name)
-  else
+    else
     raise Sinatra::NotFound
-  end
+    end
 end
--- config.ru
+++ config.ru
@@ -6,6 +6,7 @@ require 'pony'
 
 use Rack::Recaptcha, :public_key => '6LecUBUUAAAAAHQIWeMjN6W8cyOsSEmdqc-l_Tg0', :private_key => '6LecUBUUAAAAAL_OScUjFrpU2KETzXWmmRUq3MDc'
 helpers Rack::Recaptcha::Helpers
+enable :sessions
 
 require './application'
 run Sinatra::Application
