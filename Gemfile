3f2ba32f1997fdd499a0cc09449991fe0dae1300
WhpHenry <527whp@gmail.com>
2017-02-14 13:36:03 +0800

    add contact form

--- Gemfile
+++
@@ -1,8 +0,0 @@
-source "https://rubygems.org"
-# source 'http://ruby.taobao.org'
-ruby '2.2.6'
-gem 'rack'
-gem 'rack-recaptcha'
-gem 'sinatra'
-gem 'pony'
-gem 'json'
--- contact.js
+++ contact.js
@@ -1,50 +1,66 @@
-function showRecaptcha(element) {
-  Recaptcha.create('6LecUBUUAAAAAHQIWeMjN6W8cyOsSEmdqc-l_Tg0', element, {
-    theme: 'custom', // you can pick another at https://developers.google.com/recaptcha/docs/customization
-    custom_theme_widget: 'g-recaptcha'
-  });
+//update this with your js_form selector
+var form_id_js = "email_form";
+
+var data_js = {
+    "access_token": "3qgdoyil0v6j3893sq7biq9o"
+};
+
+function js_onSuccess() {
+    // remove this to avoid redirect
+    console.log("Email sent successfully");
+    window.location = window.location.pathname + "?message=Email+Successfully+Sent%21&isError=0";
 }
 
-function setupRecaptcha() {
-  var contactFormHost = 's4gpage.herokuapp.com',
-      form = $('#contact-form');
-      // notice = form.find('#notice');
+function js_onError(error) {
+    console.log("Email could not be sent Error = 1");
+    // remove this to avoid redirect
+    // window.location = window.location.pathname + "?message=Email+could+not+be+sent.&isError=1";
+}
 
-  if (form.length) {
-    // showRecaptcha('g-recaptcha');
+var sendButton = document.getElementById("js_send");
 
-    form.submit(function(ev){
-      ev.preventDefault();
-
-      $.ajax({
-        type: 'POST',
-        url: contactFormHost + 'send_email',
-        data: form.serialize(),
-        dataType: 'json',
-        success: function(response) {
-          switch (response.message) {
-            case 'success':
-              form.fadeOut(function() {
-                form.html('<h4>' + form.data('success') + '</h4>').fadeIn();
-              });
-              break;
-
-            case 'failure_captcha':
-              console.log('failure_captcha');
-              // showRecaptcha('recaptcha_widget');
-              // notice.text(notice.data('captcha-failed')).fadeIn();
-              break;
-
-            case 'failure_email':
-              console.log('failure_email');
-              // notice.text(notice.data('error')).fadeIn();
-          }
-        },
-        error: function(xhr, ajaxOptions, thrownError) {
-          console.log('error');
-          // notice.text(notice.data('error')).fadeIn();
+function js_send() {
+    sendButton.value = 'Sending…';
+    sendButton.disabled = true;
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function() {
+        if (request.readyState == 4 && request.status == 200) {
+            js_onSuccess();
+        } else
+        if (request.readyState == 4) {
+            js_onError(request.response);
         }
-      });
-    });
-  }
+    };
+
+    var fname = document.querySelector("#" + form_id_js + " [name='fName']").value;
+    var lname = document.querySelector("#" + form_id_js + " [name='lName']").value;
+    var email = document.querySelector("#" + form_id_js + " [name='email']").value;
+    var message = document.querySelector("#" + form_id_js + " [name='message']").value;
+    // data_js['user'] = (fname + lname);
+    data_js['subject'] = email;
+    data_js['text'] = ("from: "+ fname + lname + "\n" +"command: "+ message);
+    var params = toParams(data_js);
+
+    request.open("POST", "https://postmail.invotes.com/send", true);
+    request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+
+    request.send(params);
+
+    return false;
 }
+
+sendButton.onclick = js_send;
+
+function toParams(data_js) {
+    var form_data = [];
+    for (var key in data_js) {
+        form_data.push(encodeURIComponent(key) + "=" + encodeURIComponent(data_js[key]));
+    }
+
+    return form_data.join("&");
+}
+
+var js_form = document.getElementById(form_id_js);
+js_form.addEventListener("submit", function(e) {
+    e.preventDefault();
+});
